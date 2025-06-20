c0bd9f26735f9d2b3eca46be5d643f93f1c580ae
rohitkumar91131 <rk34190100@gmail.com>
2025-06-20 18:58:52 +0530

    check

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,19 @@
 import { useEffect } from "react"
 import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify"
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export default function ProtectedRoute ({children}){
     const navigate = useNavigate();
     useEffect(()=>{
         async function check_Authentication (){
             try{
-                let res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/chat/verifyLogin`,{
+                let res = await fetch(`${BACKEND_URL}/verifyToken`,{
                     method : "GET",
                     credentials : "include"
                 })
                 let data = await res.json();
+                console.log(data)
                 if(data.success){
                     navigate("/");
                 }
